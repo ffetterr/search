d08119cbe773a0b40d2622fc43fe59f2eca1649b
kirill <swiridow.kir@yandex.ru>
2022-07-14 02:19:34 +0300

    Добавил комментарии.

--- bin_searsh.py
+++ bin_searsh.py
@@ -6,17 +6,17 @@ print('Числа, которые находятся в списке через 
 for element in lst:
     print(element, end = ', ')
 digit = int(input('Введите число, индекс которого вы хотите узнать: '))
-mid = len(lst) // 2
-low = 0 
-high = len(lst) - 1
+mid = len(lst) // 2 # получаем индекс срединного элемента в списке
+low = 0 # индекс первого
+high = len(lst) - 1 # индекс последнего. -1, т.к. индексы от 0.
 while lst[mid] != digit and low <= high:
     if digit > lst[mid]:
-        low = mid + 1
+        low = mid + 1 # нижнюю границу смещаем к бывшей середине + 1
     else:
-        high = mid - 1
-    mid = (low + high) // 2
+        high = mid - 1 # иначе верхнюю границу смещаем к бывшей середине - 1
+    mid = (low + high) // 2 # обновляем значение индекса середины
 
-if low > high:
+if low > high: # для этого +1 и -1 при переприсваивании low и high, а т.ж. условие low<=high
     print("Такого числа в списке нет.")
 else:
     print("Индекс искомого числа в списке:", mid)
