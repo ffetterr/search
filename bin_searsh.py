4c15173f9ed438a8fbebc76da5ef07200f961895
kirill <swiridow.kir@yandex.ru>
2022-07-14 02:17:08 +0300

    Сделал сам бинарный поиск.

--- bin_searsh.py
+++ bin_searsh.py
@@ -5,3 +5,18 @@ lst.sort() # сортируем список
 print('Числа, которые находятся в списке через запятую:',end = ' ')
 for element in lst:
     print(element, end = ', ')
+digit = int(input('Введите число, индекс которого вы хотите узнать: '))
+mid = len(lst) // 2
+low = 0 
+high = len(lst) - 1
+while lst[mid] != digit and low <= high:
+    if digit > lst[mid]:
+        low = mid + 1
+    else:
+        high = mid - 1
+    mid = (low + high) // 2
+
+if low > high:
+    print("Такого числа в списке нет.")
+else:
+    print("Индекс искомого числа в списке:", mid)
